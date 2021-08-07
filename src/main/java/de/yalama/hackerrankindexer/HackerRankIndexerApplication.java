b5bcbdd9d6341daa8065a825cec9fcfccf2ffac9
aliyalama@googlemail.com <Gundam1979!>
2021-08-07 15:32:46 +0200

    Initial commit

---
+++ src/main/java/de/yalama/hackerrankindexer/HackerRankIndexerApplication.java
@@ -0,0 +1,13 @@
+package de.yalama.hackerrankindexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerRankIndexerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackerRankIndexerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/yalama/hackerrankindexer/HackerRankIndexerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.yalama.hackerrankindexer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerRankIndexerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

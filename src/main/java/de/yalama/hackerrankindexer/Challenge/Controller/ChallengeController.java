5a6f7e96c03c3e78b64edd4d1fc67ebf1d6069e6
aliyalama@googlemail.com <Gundam1979!>
2021-09-26 20:11:29 +0200

    Adjustments with httpSession - it now compiles

--- src/main/java/de/yalama/hackerrankindexer/Challenge/Controller/ChallengeController.java
+++ src/main/java/de/yalama/hackerrankindexer/Challenge/Controller/ChallengeController.java
@@ -25,10 +25,16 @@ public class ChallengeController implements BaseController<Challenge, Long> {
     @Autowired
     private SessionService sessionService;
 
+    //TODO diesen endpunkt schliessen
     @Override
-    @GetMapping
+    @GetMapping("/all")
     public List<Challenge> findAll() {
         return this.challengeService.findAll();
+    }
+
+    @GetMapping()
+    public List<Challenge> findAllBySession(HttpServletRequest httpServletRequest) {
+        return this.challengeService.getAllChallengesBySessionId(this.getSessionId(httpServletRequest));
     }
 
     @Override
@@ -54,25 +60,20 @@ public class ChallengeController implements BaseController<Challenge, Long> {
     public Long delete(@PathVariable Long id) throws HackerrankIndexerException {
         return this.challengeService.deleteById(id);
     }
-    //TODO
+
     @GetMapping("/{id}/submissions")
-    public Set<Submission> findSubmissionsByChallengeId(@PathVariable Long id) {
-        return this.challengeService.getSubmissionsByChallengeId(id);
+    public Set<Submission> findSubmissionsByChallengeId(@PathVariable Long id, HttpServletRequest httpServletRequest) {
+        return this.challengeService.getSubmissionsByChallengeIdAndSessionId(id, this.getSessionId(httpServletRequest));
     }
-    //TODO
+
     @GetMapping("/{id}/ispassed")
-    public Boolean checkIsSubmissionPassed(@PathVariable Long id) {
-        return this.challengeService.checkIsChallengePassed(id);
+    public Boolean checkIsSubmissionPassed(@PathVariable Long id, HttpServletRequest httpServletRequest) {
+        return this.challengeService.checkIsChallengePassedBySessionId(id, this.getSessionId(httpServletRequest));
     }
-    //TODO
+
     @GetMapping("/passed")
-    public List<Challenge> getPassedChallenges() {
-        return this.challengeService.getAllPassedChallenges();
-    }
-    //TODO
-    @GetMapping("/failed")
-    public List<Challenge> getFailedChallenges() {
-        return this.challengeService.getAllFailedChallenges();
+    public List<Challenge> getPassedChallenges(HttpServletRequest httpServletRequest) {
+        return this.challengeService.getAllPassedChallengesBySessionId(this.getSessionId(httpServletRequest));
     }
 
     private long getSessionId(HttpServletRequest httpServletRequest) {

420da2f50bca7023106ebb4b37e2f7add1f082e0
aliyalama@googlemail.com <Gundam1979!>
2022-01-13 06:28:40 +0100

    init more stuff

--- FE/src/app/app.module.ts
+++ FE/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import {AppRoutingModule} from "./app.routing-module";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

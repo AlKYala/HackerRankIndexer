d9127f57dc5a9ebc073f7ae7009b47a3f5dd3040
aliyalama@googlemail.com <Gundam1979!>
2022-02-20 15:09:30 +0100

    Added @RequestBody parameter so it can be found in the request body

--- src/main/java/de/yalama/hackerrankindexer/Submission/Controller/SubmissionController.java
+++ src/main/java/de/yalama/hackerrankindexer/Submission/Controller/SubmissionController.java
@@ -73,7 +73,7 @@ public class SubmissionController implements BaseController<Submission, Long> {
     }
 
     @GetMapping("/filter")
-    public Collection<Submission> getFilterRequest(HttpServletRequest httpServletRequest, FilterRequest filterRequest) {
+    public Collection<Submission> getFilterRequest(HttpServletRequest httpServletRequest, @RequestBody  FilterRequest filterRequest) {
         User user = this.headerService.getUserFromHeader(httpServletRequest);
         return this.submissionService.getByFilterRequest(filterRequest, user);
     }
